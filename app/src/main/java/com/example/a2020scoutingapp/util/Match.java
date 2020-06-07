6742076d707dd92d9e1cec8984b5240f731f3edb
aamathur02 <aamathur02@gmail.com>
2020-06-06 23:39:41 -0400

    added Parcelable implementation to allow Match class to be passed in Bundle.

--- app/src/main/java/com/example/a2020scoutingapp/util/Match.java
+++ app/src/main/java/com/example/a2020scoutingapp/util/Match.java
@@ -1,10 +1,44 @@
 package com.example.a2020scoutingapp.util;
 
-public class Match {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Match implements Parcelable {
 
     public int robotNumber;
     public int matchNumber;
     public String scouterName;
 
     //TODO: add additional fields for match info
+
+
+    protected Match(Parcel in) {
+        robotNumber = in.readInt();
+        matchNumber = in.readInt();
+        scouterName = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(robotNumber);
+        dest.writeInt(matchNumber);
+        dest.writeString(scouterName);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Match> CREATOR = new Creator<Match>() {
+        @Override
+        public Match createFromParcel(Parcel in) {
+            return new Match(in);
+        }
+
+        @Override
+        public Match[] newArray(int size) {
+            return new Match[size];
+        }
+    };
 }
